5c638cf758d12d818ebfa1443c9c740df777f6ed
smallcjy <2628035541@qq.com>
2024-07-29 17:28:16 +0800

    0729

--- Leetcode_cpp/19.remove-nth-node-from-end-of-list.cpp
+++ Leetcode_cpp/19.remove-nth-node-from-end-of-list.cpp
@@ -5,7 +5,6 @@
  * [19] 删除链表的倒数第 N 个结点
  */
 
-
 // @lcpr-template-start
 using namespace std;
 #include <algorithm>
@@ -35,15 +34,47 @@ using namespace std;
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
 
+// struct ListNode
+// {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+
+class Solution
+{
+public:
+//指针列表的删除需要单独考虑删除头指针和尾指针
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        unordered_map<int, ListNode*> map;
+        ListNode* cur = head;
+        int i = 1;
+        while(cur!=nullptr) {
+            map.emplace(i, cur);
+            cur = cur->next;
+            i++;
+        }
+        i--;
+        std::cout<<i;
+        int pos = i - n;
+        if(pos == 0) {
+           head = head->next;
+        }else if(pos == i) {
+           ListNode* node = map.at(pos);
+           node->next = nullptr;
+        }else {
+            ListNode* node = map.at(pos);
+            node->next = node->next->next;
+        }
+
+        return head;
     }
 };
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -55,8 +86,7 @@ public:
 // @lcpr case=end
 
 // @lcpr case=start
-// [1,2]\n1\n
+// [1,2]\n2\n
 // @lcpr case=end
 
  */
-
