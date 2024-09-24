2a06fab678f784bec78f9bc90748ef0a8e09ae20
smallcjy <2628035541@qq.com>
2024-09-24 10:04:07 +0800

    0924

---
+++ Leetcode_cpp/45.jump-game-ii.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=45 lang=cpp
+ * @lcpr version=30204
+ *
+ * [45] 跳跃游戏 II
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0, currentEnd = 0, farthest = 0;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            farthest = max(farthest, i + nums[i]);
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+        return jumps;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [2,3,1,1,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [2,3,0,1,4]\n
+// @lcpr case=end
+
+ */
+
